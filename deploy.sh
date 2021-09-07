5bf1f249c8236c1b634b0bf87091fde6b1954a74
Flávio Nogueira Nery <flavio8163@gmail.com>
2021-09-07 12:50:47 -0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:flavionn/2021-gulacerta-app-vue.git main:gh-pages
 
